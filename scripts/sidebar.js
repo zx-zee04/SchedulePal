4e3d3d3119999cae34af10586a34455ab348360b
Kalingga Dwindra Putraka <kalingga221@gmail.com>
2024-11-09 22:45:41 +0800

    sidebar active

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,6 +1,10 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll("nav a").forEach((link) => {
-  if (link.href.includes(`${activePage}`)) {
+const activePage = window.location.href; // Mengambil URL lengkap
+const navLinks = document.querySelectorAll("nav a");
+navLinks.forEach((link) => {
+  link.classList.remove("active");
+});
+navLinks.forEach((link) => {
+  if (link.href === activePage) {
     link.classList.add("active");
   }
 });
