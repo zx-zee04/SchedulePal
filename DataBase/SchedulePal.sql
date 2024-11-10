fd1d1682483df5ba37f594a1f3833d15a6f4ddf1
EXOGAMER007 <daffa8110@gmail.com>
2024-11-10 22:43:42 +0800

    update struktur

--- DataBase/SchedulePal.sql
+++ DataBase/SchedulePal.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `schedulepal` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `schedulepal`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: schedulepal
@@ -106,4 +104,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-10 22:34:58
+-- Dump completed on 2024-11-10 22:42:47
