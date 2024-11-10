25820d7dfa82b88760b19b8a350ba7c95911e25c
Kalingga Dwindra Putraka <kalingga221@gmail.com>
2024-11-10 22:30:00 +0800

    schedule request

--- requests.php
+++ requests.php
@@ -37,10 +37,58 @@
                     <td>Waktu</td>
                     <td>Tanggal</td>
                     <td>
-                        <a href=""><button></button></a>
+                        <a href="">
+                            <button><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#4ADE80"><path d="m424-312 282-282-56-56-226 226-114-114-56 56 170 170ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Z"/></svg></button>
+                        </a>
+                        <a href="">
+                            <button><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EF4444"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Z"/></svg></button>
+                        </a>
                     </td>
                 </tr>
             </tbody>
+        </table>
+        <div class="bottom-container">
+            <p>Showing 1 of 0</p>
+            <div class="pagination">
+                <button type="button">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="12"
+                    viewBox="0 0 24 24"
+                    >
+                    <path
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m14 7l-5 5l5 5"
+                    />
+                    </svg>
+                </button>
+                <div>Page 1 of 10</div>
+                <button type="button">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="12"
+                    viewBox="0 0 24 24"
+                    >
+                    <g transform="translate(24 0) scale(-1 1)">
+                        <path
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m14 7l-5 5l5 5"
+                        />
+                    </g>
+                    </svg>
+                </button>
+            </div>
+        </div>
     </div>
 </body>
 </html>
