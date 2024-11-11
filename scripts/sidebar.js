e2106e7e68d0f13e44bbcdc0586e0d884e1f3a52
Kalingga Dwindra Putraka <kalingga221@gmail.com>
2024-11-11 10:13:10 +0800

    sidebar active

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -5,7 +5,8 @@ navLinks.forEach((link) => {
   link.classList.remove("active");
 });
 navLinks.forEach((link) => {
-  console.log(link.href.split("/")[3]);
+  console.log(link.href);
+  console.log(link.href.split("/"));
   if (activePage.startsWith(`/${link.href.split("/")[3]}`)) {
     link.classList.add("active");
   }
